a93ae058b09c1df5bc0110ec797274ce34a0f3b8
hamzahamruni <hamzahamroni95@gmail.com>
2024-02-12 11:27:58 +0200

    24-02-10

--- include/db_connection.php
+++ include/db_connection.php
@@ -1,11 +1,8 @@
 <?php
-	// @define("DB_SERVER" , "localhost:3306");
-	// @define("DB_USER" , "us_sror");
-	// @define("DB_PASS" , "#c2yY9d75");
-	// @define("DB_NAME" , "sror");
+
 	@define("DB_SERVER" , "localhost:3306");
 	@define("DB_USER" , "root");
-	@define("DB_PASS" , "Q7Q9754RTZ:>z;k&LG(AU0j615Jv_9");
+	@define("DB_PASS" , "root");
 	@define("DB_NAME" , "sror");
 	$connection = mysqli_connect(DB_SERVER, DB_USER, @DB_PASS, DB_NAME);
 	$connection->set_charset("utf8");
